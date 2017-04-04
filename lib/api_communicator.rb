bbeaea5ab027288f404f9baf9a53d128f160cec4
github username <github email address>
2017-04-04 15:13:09 -0400

    edit to parse_character_movies

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -29,13 +29,14 @@ def get_character_movies_from_api(character)
 end
 
 def parse_character_movies(films_hash)
-  character each.do |character|
-  puts "#{character[:results][:name]}: #{character[:results][:films]}"
-  #.parse.with_index(1)
-
-  puts character
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts "#{film['title']}"
+  end
 end
+
+
+  # some iteration magic and puts out the movies in a nice list
+
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
